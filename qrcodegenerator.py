a0950219a95f63efc5e4eb428c096bf47a82c6b8
sirivarshini-954 <sirivarshini.pentakota@gmail.com>
2025-01-11 14:38:06 +0530

    Add files via upload
    *QR Code Generator: Generates custom QR codes for URLs or text.

---
+++ qrcodegenerator.py
@@ -0,0 +1,14 @@
+import qrcode
+import image
+qr = qrcode.QRCode( 
+    version = 15,
+    box_size = 10,
+    border = 5
+)
+
+data="https://www.linkedin.com/in/siri-varshini-pentakota-10054b254/"
+
+qr.add_data(data)
+qr.make(fit = True)
+img = qr.make_image(fill = "black", back_color = "white")
+img.save("test.png")
